13d6e98bb609a76bc7fcbe082eb835689c1cd66f
Matthew Peters <matthewp@allenai.org>
2017-12-01 16:05:15 -0800

    use_skip_connections is now required in the options file

--- bilm/model.py
+++ bilm/model.py
@@ -480,10 +480,11 @@ class BidirectionalLanguageModelGraph(object):
         n_lstm_layers = self.options['lstm'].get('n_layers', 1)
         cell_clip = self.options['lstm'].get('cell_clip')
         proj_clip = self.options['lstm'].get('proj_clip')
-        use_skip_connections = self.options['lstm'].get(
-                                            'use_skip_connections')
+        use_skip_connections = self.options['lstm']['use_skip_connections']
         if use_skip_connections:
             print("USING SKIP CONNECTIONS")
+        else:
+            print("NOT USING SKIP CONNECTIONS")
 
         # the sequence lengths from input mask
         if self.use_character_inputs:
